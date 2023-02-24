6c529a7b6c6fd2c1bb047458b763f37721e43168
Sanidhya-30 <sanidhya3009@gmail.com>
2023-02-24 19:01:23 +0530

    corrected f/b parameters

--- src/dock.py
+++ src/dock.py
@@ -6,17 +6,17 @@ from .Camera import camera
 length = 45
 breadth = 30
 
-def coverForwardArea(rover, d, spd):
-    rover.moveForward(x=d,speed=spd)
+def coverForwardArea(rover, spd):
+    rover.moveForward(speed=spd)
 
-def coverBackwardArea(rover, d, spd):
-    rover.moveBackward(x=d,speed=spd)
+def coverBackwardArea(rover, spd):
+    rover.moveBackward(speed=spd)
 
 def changeDirection(rover, angle):
         rover.changeYaw(angle=angle,speed=0.02)
 
 
-def dock(rover): #take frame and avgX as argument
+def dock(rover):
     
     label_font = cv2.FONT_HERSHEY_SIMPLEX
    
@@ -46,34 +46,6 @@ def dock(rover): #take frame and avgX as argument
             cv2.putText(masked_image, "Move Left", (50, 50), label_font, 0.5, (255, 0, 0), 2)
             coverForwardArea(rover,spd=2)
             print("Moving Left by",drift)
-            
-
-# def changeLane(rover,drift=breadth):
-    
-#     H = math.sqrt(((length/2)**2)+(drift**2))
-#     theta = math.atan((drift)/(length/2))
-
-#     try:
-#         while(rover.back_edge.checkDriveOk() == True):
-            
-#             coverForwardArea(rover,d=int((drift/2)),spd=2)
-#             changeDirection(rover, theta)
-            
-#             if (rover.front_edge.checkDriveOk() == True):
-#             # Lane End
-#                 changeDirection(rover, -theta)
-#                 coverBackwardArea(rover,d=int((drift/2)),spd=2)
-#                 #check drone status
-#                 #call dock function()
-#                 break
-
-#             else:
-#                 coverForwardArea(rover,d=int((H)),spd=2)
-#                 changeDirection(rover, (-theta))
-#                 coverBackwardArea(rover,d=int((3*drift)/2),spd=2)
-
-#     except KeyboardInterrupt:
-#         keyboard_shutdown() 
 
 
 if __name__ == '__main__':
