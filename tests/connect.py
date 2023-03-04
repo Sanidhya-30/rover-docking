b813e1e0ea578c6df5e5c37d869bd17d5afc7be4
akshat <akshatbhatia05@gmail.com>
2023-03-04 21:19:29 +0530

    yaw data

--- tests/connect.py
+++ tests/connect.py
@@ -14,21 +14,30 @@ print("Heartbeat from system (system %u component %u)" %
 
 while 1:
       # msg = the_connection.recv_match(type='ATTITUDE', blocking=True)
-      #pos = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+      # pos = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
 
       # distance to next waypoint
       
-      the_connection.mav.request_data_stream_send(the_connection.target_system, the_connection.target_component,
-                                        mavutil.mavlink.MAV_DATA_STREAM_ALL, 2, 1)
+      the_connection.mav.command_long_encode(
+		0, 0,
+		mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+		0,
+		30,
+		0,
+		0,
+		0,
+		0,0, 0)
       #msg = the_connection.recv_match(type='MAV',blocking=True)
-      data = the_connection.messages
-      #system = the_connection.recv_match(type='SYS_STATUS', blocking=True)
-      print(data)
+      data = the_connection.messages.keys()
+      system = the_connection.recv_match(type='ATTITUDE', blocking=True)
+      # print(data)
+      
       the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, the_connection.target_system,
                         the_connection.target_component, mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED , int(0b100111100111), 0, 0, 0, 0.2, 0, 0, 0, 0, 0, 0.8, 0))
       
       time.sleep(1)
-      
-      data = the_connection.messages['HOME']
-      print(data)
+      pos = the_connection.recv_match(type='GPS_RAW_INT', blocking=True)
+      # print(system.yaw)
+      print(pos)
+      # data = the_connection.messages['HOME']
       time.sleep(2)
