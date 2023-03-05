6172930b7d8a6135f9547fcd3ffdebae527825f4
akshat <akshatbhatia05@gmail.com>
2023-03-05 17:19:57 +0530

    comment

--- src/dock.py
+++ src/dock.py
@@ -41,37 +41,37 @@ def dock(rover:Rover):
     rover.changeYaw(angle=-math.radians(90), speed=0.1)
     print('Done!!!!!')
    
-    # while True:
+    while True:
         
-    #     dock_x, masked_image = rover.camera.capture()
-    #     masked_image = cv2.rotate(masked_image, cv2.ROTATE_180)
-    #     #cv2.imshow('OG', src_image)
-    #     FrameCenter_X = round(masked_image.shape[1]/2)
-    #     FrameCenter_Y = round(masked_image.shape[0]/2)
-    #     masked_image = cv2.circle(masked_image, (FrameCenter_X, FrameCenter_Y), radius=10, color=(255, 0, 0), thickness=-1)
-    #     drift = (dock_x-FrameCenter_X)
-    #     # print('drift', drift)
-    #     K=1
+        dock_x, masked_image = rover.camera.capture()
+        masked_image = cv2.rotate(masked_image, cv2.ROTATE_180)
+        #cv2.imshow('OG', src_image)
+        FrameCenter_X = round(masked_image.shape[1]/2)
+        FrameCenter_Y = round(masked_image.shape[0]/2)
+        masked_image = cv2.circle(masked_image, (FrameCenter_X, FrameCenter_Y), radius=10, color=(255, 0, 0), thickness=-1)
+        drift = (dock_x-FrameCenter_X)
+        # print('drift', drift)
+        K=1
         
-    #     if dock_x is not 0:
+        if dock_x is not 0:
             
-    #         if (drift) > 25:
-    #             cv2.putText(masked_image, "Move Left", (50, 50), label_font, 0.5, (255, 0, 0), 2)
-    #             align(rover, (K*drift))
+            if (drift) > 25:
+                cv2.putText(masked_image, "Move Left", (50, 50), label_font, 0.5, (255, 0, 0), 2)
+                align(rover, (K*drift))
 
-    #         elif (drift) < -25:
-    #             cv2.putText(masked_image, "Move Right", (50, 50), label_font, 0.5, (255, 0, 0), 2)
-    #             align(rover, (-K*drift))
+            elif (drift) < -25:
+                cv2.putText(masked_image, "Move Right", (50, 50), label_font, 0.5, (255, 0, 0), 2)
+                align(rover, (-K*drift))
 
-    #         elif -25 < (drift) < 25 :
-    #             cv2.putText(masked_image, "Move Forward", (50, 50), label_font, 0.5, (255, 0, 0), 2)
-    #             moveF(rover,spd=2)
+            elif -25 < (drift) < 25 :
+                cv2.putText(masked_image, "Move Forward", (50, 50), label_font, 0.5, (255, 0, 0), 2)
+                moveF(rover,spd=2)
         
-    #     else:
-    #         print("Drone not detected")
+        else:
+            print("Drone not detected")
         
-    #     cv2.imshow('masked', masked_image)
-    #     cv2.waitKey(1)
+        cv2.imshow('masked', masked_image)
+        cv2.waitKey(1)
 
 
 if __name__ == '__main__':
